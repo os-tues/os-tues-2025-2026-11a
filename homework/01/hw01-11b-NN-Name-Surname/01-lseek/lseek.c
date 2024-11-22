3cf509c6c48a31ea919e3ed2f95bbd8c89b45abc
IvayloBozhilov <ibozhilov.elsys@gmail.com>
2024-11-22 19:56:28 +0200

    docs: homework1 updated

--- homework/01/hw01-11b-NN-Name-Surname/01-lseek/lseek.c
+++ homework/01/hw01-11b-NN-Name-Surname/01-lseek/lseek.c
@@ -1,5 +1,20 @@
-int main(int argc, char const *argv[])
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+typedef struct
 {
-    /* code */
+    char data;
+    unsigned char nextElementAddress;
+} block;
+
+int main(int argc, char* argv[]){
+
+    //code
+
     return 0;
 }
