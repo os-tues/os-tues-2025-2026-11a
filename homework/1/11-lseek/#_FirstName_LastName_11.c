52c8d2181681e3c150c9f0827fe0a7ef5fbd3583
Ivaylo Bozhilov <ibozhilov.elsys@gmail.com>
2025-11-23 19:29:48 +0200

    feat: hw01

---
+++ homework/1/11-lseek/#_FirstName_LastName_11.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+typedef struct
+{
+    char data;
+    unsigned char nextElementAddress;
+} block;
+
+int main(int argc, char* argv[]){
+
+    //code
+
+    return 0;
+}
---
+++ homework/1/12-tail/#_FirstName_LastName_12.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdbool.h>
+
+
+int main(int argc, char* argv[]) {
+
+    //code
+
+    return 0;
+}
